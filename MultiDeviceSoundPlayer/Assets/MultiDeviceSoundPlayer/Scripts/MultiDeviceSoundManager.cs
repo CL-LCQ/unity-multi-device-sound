f5d887b390906f3e8866f5605afa42fbe0980797
hirazawa satoshi <hirazawa-satoshi@kayac.com>
2020-03-03 00:06:58 +0900

    add project file

---
+++ MultiDeviceSoundPlayer/Assets/MultiDeviceSoundPlayer/Scripts/MultiDeviceSoundManager.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Runtime.InteropServices;
+
+namespace MultiDeviceSoundPlayer {
+
+    public class MultiDeviceSoundManager {
+
+        private IntPtr _instance;
+
+        [DllImport ("MultiDeviceSoundPlayer", EntryPoint="MultiSoundDviceInterface_create")]
+        private static extern IntPtr NativeCreate();
+
+        [DllImport ("MultiDeviceSoundPlayer", EntryPoint="MultiSoundDviceInterface_destroy")]
+        private static extern void NativeDestroy(IntPtr instance);
+
+        [DllImport ("MultiDeviceSoundPlayer", EntryPoint="MultiSoundDviceInterface_play")]
+        private static extern void NativePlay(IntPtr instance);
+
+        [DllImport ("MultiDeviceSoundPlayer", EntryPoint="MultiSoundDviceInterface_loadSoundWithTarget")]
+        private static extern int NativeLoad(IntPtr instance, string fileName, int deviceID);
+
+        [DllImport ("MultiDeviceSoundPlayer", EntryPoint="MultiSoundDviceInterface_getDeviceInfo")]
+        private static extern string NativeGetDevice(IntPtr instance, int deviceID);
+
+        public MultiDeviceSoundManager() {
+            _instance = NativeCreate();
+        }
+
+        public void Destroy() {
+            if (IsDestroyed()) {
+                return;
+            }
+            NativeDestroy(_instance);
+            _instance = IntPtr.Zero;
+        }
+
+        private bool IsDestroyed() {
+            return _instance == IntPtr.Zero;
+        }
+
+        public int Load(string fileName, int deviceID) {
+            if (IsDestroyed()) {
+                return -1;
+            }
+            return NativeLoad(_instance, fileName, deviceID);
+        }
+
+        public void Play() {
+            if (IsDestroyed()) {
+                return;
+            }
+            NativePlay(_instance);
+        }
+
+        public string GetDveice(int id){
+            if (IsDestroyed()) {
+                return "Instance does not exist.";
+            }
+            return NativeGetDevice(_instance, id);
+        }
+    }
+}
---
+++ MultiDeviceSoundPlayer/Assets/MultiDeviceSoundPlayer/Scripts/SoundManager.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using MultiDeviceSoundPlayer;
+
+public class SoundManager : MonoBehaviour{
+
+    MultiDeviceSoundManager soundPlayer;
+    MultiDeviceSoundManager soundPlayer2;
+
+    [SerializeField]Text deviceList;
+
+    // Start is called before the first frame update
+    void Start(){
+        soundPlayer = new MultiDeviceSoundManager();
+        soundPlayer2 = new MultiDeviceSoundManager();
+
+        deviceList.text = "deviceList:\n" + soundPlayer.GetDveice(0);
+
+
+        soundPlayer.Load(Application.streamingAssetsPath + "/Audios/test1.mp3", 0);        
+        soundPlayer2.Load(Application.streamingAssetsPath + "/Audios/test2.mp3", 0);
+
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+        if(Input.GetKeyDown(KeyCode.Alpha0)){
+            soundPlayer.Play();
+        }
+
+        if(Input.GetKeyDown(KeyCode.Alpha2)){
+            soundPlayer2.Play();
+        }
+    }
+
+    private void OnApplicationQuit(){
+        soundPlayer.Destroy();
+        soundPlayer2.Destroy();
+    }
+}
